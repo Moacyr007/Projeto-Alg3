50063e6b2dbf01f9cb738ee7ff375c291098b16c
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2017-08-18 18:09:45 -0400

    Não sei o que está acontecendo

--- src/br/ufmt/ic/alg3/videoJogos/view/ProcurarAmigos.java
+++ src/br/ufmt/ic/alg3/videoJogos/view/ProcurarAmigos.java
@@ -2,6 +2,34 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */2/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package br.ufmt.ic.alg3.videoJogos.view;
 
@@ -29,8 +57,8 @@ public class ProcurarAmigos extends javax.swing.JPanel {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaProcurarAmigosjTable = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
-        conteudoPrincipaljLabel = new javax.swing.JLabel();
+        conteudoProcurarAmigosjLabel = new javax.swing.JLabel();
+        conteudoProcurarAmigos2jLabel = new javax.swing.JLabel();
 
         setMinimumSize(new java.awt.Dimension(600, 450));
         setRequestFocusEnabled(false);
@@ -38,15 +66,15 @@ public class ProcurarAmigos extends javax.swing.JPanel {
         tabelaProcurarAmigosjTable.setModel(new ProcurarAmigosTableModel(banco.getAll()));
         jScrollPane1.setViewportView(tabelaProcurarAmigosjTable);
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 200));
-        jLabel1.setText("Procurar amigos");
+        conteudoProcurarAmigosjLabel.setFont(new java.awt.Font("Segoe UI Black", 0, 24)); // NOI18N
+        conteudoProcurarAmigosjLabel.setForeground(new java.awt.Color(0, 0, 200));
+        conteudoProcurarAmigosjLabel.setText("Procurar amigos");
 
-        conteudoPrincipaljLabel.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
-        conteudoPrincipaljLabel.setForeground(new java.awt.Color(200, 0, 0));
-        conteudoPrincipaljLabel.setText("Encontre amigos para socializar e se divertir!");
-        conteudoPrincipaljLabel.setAlignmentX(0.5F);
-        conteudoPrincipaljLabel.setBorder(new javax.swing.border.MatteBorder(null));
+        conteudoProcurarAmigos2jLabel.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
+        conteudoProcurarAmigos2jLabel.setForeground(new java.awt.Color(200, 0, 0));
+        conteudoProcurarAmigos2jLabel.setText("Encontre amigos para socializar e se divertir!");
+        conteudoProcurarAmigos2jLabel.setAlignmentX(0.5F);
+        conteudoProcurarAmigos2jLabel.setBorder(new javax.swing.border.MatteBorder(null));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -57,8 +85,8 @@ public class ProcurarAmigos extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(24, 24, 24)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(conteudoPrincipaljLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(conteudoProcurarAmigosjLabel)
+                            .addComponent(conteudoProcurarAmigos2jLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(60, 60, 60)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -68,9 +96,9 @@ public class ProcurarAmigos extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addComponent(conteudoProcurarAmigosjLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(conteudoPrincipaljLabel)
+                .addComponent(conteudoProcurarAmigos2jLabel)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(24, 24, 24))
@@ -79,8 +107,8 @@ public class ProcurarAmigos extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel conteudoPrincipaljLabel;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel conteudoProcurarAmigos2jLabel;
+    private javax.swing.JLabel conteudoProcurarAmigosjLabel;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabelaProcurarAmigosjTable;
     // End of variables declaration//GEN-END:variables
