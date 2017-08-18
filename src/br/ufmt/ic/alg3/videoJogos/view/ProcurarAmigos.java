0aacf0682a5d9e8d653d570d464fb5d9e0024274
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2017-08-18 14:28:20 -0400

    Modificada a interface procurar amigos

--- src/br/ufmt/ic/alg3/videoJogos/view/ProcurarAmigos.java
+++ src/br/ufmt/ic/alg3/videoJogos/view/ProcurarAmigos.java
@@ -29,6 +29,8 @@ public class ProcurarAmigos extends javax.swing.JPanel {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaProcurarAmigosjTable = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        conteudoPrincipaljLabel = new javax.swing.JLabel();
 
         setMinimumSize(new java.awt.Dimension(600, 450));
         setRequestFocusEnabled(false);
@@ -36,26 +38,49 @@ public class ProcurarAmigos extends javax.swing.JPanel {
         tabelaProcurarAmigosjTable.setModel(new ProcurarAmigosTableModel(banco.getAll()));
         jScrollPane1.setViewportView(tabelaProcurarAmigosjTable);
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 200));
+        jLabel1.setText("Procurar amigos");
+
+        conteudoPrincipaljLabel.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
+        conteudoPrincipaljLabel.setForeground(new java.awt.Color(200, 0, 0));
+        conteudoPrincipaljLabel.setText("Encontre amigos para socializar e se divertir!");
+        conteudoPrincipaljLabel.setAlignmentX(0.5F);
+        conteudoPrincipaljLabel.setBorder(new javax.swing.border.MatteBorder(null));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(78, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(70, 70, 70))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(conteudoPrincipaljLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(88, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 394, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(32, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(conteudoPrincipaljLabel)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel conteudoPrincipaljLabel;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabelaProcurarAmigosjTable;
     // End of variables declaration//GEN-END:variables
