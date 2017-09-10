ed21a0da299e46226813557e1dfbf9de23c1d6e7
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2017-09-10 14:45:58 -0400

    inserido botão editar

--- src/br/ufmt/ic/alg3/videoJogos/view/cadastro/JogoJPanel.java
+++ src/br/ufmt/ic/alg3/videoJogos/view/cadastro/JogoJPanel.java
@@ -54,6 +54,7 @@ public class JogoJPanel extends javax.swing.JPanel {
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaJogosCadastrojTable = new javax.swing.JTable();
         removerjButton = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setMinimumSize(new java.awt.Dimension(800, 600));
         setPreferredSize(new java.awt.Dimension(800, 600));
@@ -117,6 +118,13 @@ public class JogoJPanel extends javax.swing.JPanel {
             }
         });
 
+        jButton1.setText("Editar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -128,8 +136,10 @@ public class JogoJPanel extends javax.swing.JPanel {
                         .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(90, 90, 90)
-                        .addComponent(removerjButton)
-                        .addGap(15, 15, 15)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(removerjButton)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 464, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(206, 206, 206)
@@ -206,7 +216,10 @@ public class JogoJPanel extends javax.swing.JPanel {
                     .addComponent(cancelarjButton, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(59, 59, 59)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(removerjButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(removerjButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -278,6 +291,25 @@ public class JogoJPanel extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_precojTextFieldActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    int linha = tabelaJogosCadastrojTable.getSelectedRow();
+
+        if (linha >= 0) {
+            Jogo selecionado;
+            selecionado = banco.getAll().get(linha);
+
+            idjTextField.setText(selecionado.getId() + "");
+            nomejTextField.setText(selecionado.getNome());
+            precojTextField.setText(selecionado.getPreco());
+            desenvolvedorjTextField.setText(selecionado.getDesenvolvedor());
+            classIndcjTextField.setText(selecionado.getClassificacaoIndicativa());
+            tipojTextField.setText(selecionado.getTipo());
+        } else {
+            JOptionPane.showMessageDialog(this, "Selecione 1 registro");
+        }
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cancelarjButton;
@@ -287,6 +319,7 @@ public class JogoJPanel extends javax.swing.JPanel {
     private javax.swing.JLabel desenvolvedornomejLabel;
     private javax.swing.JLabel idjLabel;
     private javax.swing.JTextField idjTextField;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel nomejLabel;
